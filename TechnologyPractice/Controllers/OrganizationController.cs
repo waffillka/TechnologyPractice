695b8ae3881fd5ef8fba9e9a2d887f44af765b8b
“Sebik <waffilka@gmail.comSebik Georgegit config --global user.email waffilka@gmail.com>
2021-06-15 00:21:16 +0300

    add ExceptionHandler

---
+++ TechnologyPractice/Controllers/OrganizationController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechnologyPractice.Controllers
+{
+    [Route("api/organization")]
+    [ApiController]
+    public class OrganizationController : ControllerBase
+    {
+        private readonly ILoggerManager _logger;
+        private readonly IRepositoryManager _repository;
+        private IMapper _mapper;
+
+        public OrganizationController(ILoggerManager logger, IRepositoryManager repository, IMapper mapper)
+        {
+            _logger = logger;
+            _repository = repository;
+            _mapper = mapper;
+        }
+    }
+}
