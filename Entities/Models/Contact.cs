62e1d337902320e2c18d366e203e92599b41388a
“Sebik <waffilka@gmail.comSebik Georgegit config --global user.email waffilka@gmail.com>
2021-06-14 14:50:07 +0300

    Add project files.

---
+++ Entities/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Contact
+    {
+        Guid Id;
+        string LastName;
+        string FirstName;
+        string Email;
+    }
+}
